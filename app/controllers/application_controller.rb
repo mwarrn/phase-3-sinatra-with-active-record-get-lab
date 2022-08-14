106e51b66bcef16c4337e760d0c331854208c6ec
Michael Warren <mwarrn@amazon.com>
2022-08-14 14:33:07 -0700

    Completed assignment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,29 @@
 class ApplicationController < Sinatra::Base
 
-  # add routes
+  set default_content_type: "application/json"
+  
+  # Returns all the bakeries in the database.
+  get '/bakeries' do
+    bakeries = Bakery.all
+    bakeries.to_json
+  end
+  
+  # Returns a single bakery based on the id passed in the params.
+  get '/bakeries/:id' do
+    bakery = Bakery.find(params[:id])
+    bakery.to_json(include: :baked_goods)
+  end
+
+  # Returns all the baked goods in the database sorted by price.
+  get '/baked_goods/by_price' do
+    baked_goods = BakedGood.by_price
+    baked_goods.to_json
+  end
+
+# Returns the most expensive baked good in the database.
+  get '/baked_goods/most_expensive' do
+    baked_good = BakedGood.by_price.first
+    baked_good.to_json
+  end
 
 end
--- app/models/baked_good.rb
+++ app/models/baked_good.rb
@@ -1,3 +1,14 @@
 class BakedGood < ActiveRecord::Base
-  # add association macro here
+  belongs_to :bakery
+
+  # A class method that orders all the baked goods by price.
+  def self.by_price
+    self.all.order(price: :desc)
+  end
+
+  # A class method that orders all the baked goods by price and returns the first one.
+  def self.most_expensive
+    self.all.order(price: :desc).first
+  end
+
 end
